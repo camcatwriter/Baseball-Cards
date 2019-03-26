85e2da00d1a43e1008f321843df2595424f8c8d8
camcatwriter <theothertotalbookworm@gmail.com>
2019-03-26 11:01:34 -0600

    added card function to main.js

--- main.js
+++ main.js
@@ -6,3 +6,16 @@ import { pokemon } from './pokemondata.js'
 // card.addEventListener( 'mouseover', function() {
 //   card.classList.toggle('is-flipped');  
 // });
+
+
+const mainContainer = document.querySelector('.container')
+
+function createPokeCard(pokecard) {
+  let card = document.createElement('div')
+  let name = document.createElement('p')
+
+  name.textContent = pokecard.name
+  card.appendChild(name)
+  
+  mainContainer.appendChild(card)
+}
