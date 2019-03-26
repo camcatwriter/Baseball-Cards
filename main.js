d0c9444fa7e5d6b0259f0503072065be9154f1ca
camcatwriter <theothertotalbookworm@gmail.com>
2019-03-26 10:49:15 -0600

    linking to and importing pokedata.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-var card = document.querySelector('.card');
-card.addEventListener( 'mouseover', function() {
-  card.classList.toggle('is-flipped');  
-});
+import { pokemon } from './pokemondata.js' 
+
+//var card = document.querySelector('.card');
+// card.addEventListener( 'mouseover', function() {
+//   card.classList.toggle('is-flipped');  
+// });
