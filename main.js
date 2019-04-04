8adb649c8182bd1748164d8c8d14db82eb7031b5
camcatwriter <theothertotalbookworm@gmail.com>
2019-04-04 16:29:18 -0600

    fetch function works, deleted old json

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 import { pokemon } from './pokemon.js'
+
+fetch('https://pokeapi.co/api/v2/pokemon/1')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(myJson) ;
+  })
+
 console.log(pokemon)
 
+function createPokeCard(pokedata) {
+  
+}
