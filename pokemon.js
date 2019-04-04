3bd3d91a98df1394afb3d6dbe783000cb240693c
camcatwriter <theothertotalbookworm@gmail.com>
2019-04-04 15:38:29 -0600

    replaced pokemon data to be just what i need

--- pokemon.js
+++ pokemon.js
@@ -1,491 +1,102 @@
-export const pokemon = [
-{
-    "id": 1,
-    "name": {
-      "english": "Bulbasaur",
-      "japanese": "フシギダネ",
-      "chinese": "妙蛙种子"
+[
+    {
+      "name": "bulbasaur",
+      "url": "https://pokeapi.co/api/v2/pokemon/1/"
     },
-    "type": [
-      "Grass",
-      "Poison"
-    ],
-    "base": {
-      "HP": 45,
-      "Attack": 49,
-      "Defense": 49,
-      "Sp. Attack": 65,
-      "Sp. Defense": 65,
-      "Speed": 45
-    }
-  },
-  {
-    "id": 2,
-    "name": {
-      "english": "Ivysaur",
-      "japanese": "フシギソウ",
-      "chinese": "妙蛙草"
+    {
+      "name": "ivysaur",
+      "url": "https://pokeapi.co/api/v2/pokemon/2/"
     },
-    "type": [
-      "Grass",
-      "Poison"
-    ],
-    "base": {
-      "HP": 60,
-      "Attack": 62,
-      "Defense": 63,
-      "Sp. Attack": 80,
-      "Sp. Defense": 80,
-      "Speed": 60
-    }
-  },
-  {
-    "id": 3,
-    "name": {
-      "english": "Venusaur",
-      "japanese": "フシギバナ",
-      "chinese": "妙蛙花"
+    {
+      "name": "venusaur",
+      "url": "https://pokeapi.co/api/v2/pokemon/3/"
     },
-    "type": [
-      "Grass",
-      "Poison"
-    ],
-    "base": {
-      "HP": 80,
-      "Attack": 82,
-      "Defense": 83,
-      "Sp. Attack": 100,
-      "Sp. Defense": 100,
-      "Speed": 80
-    }
-  },
-  {
-    "id": 4,
-    "name": {
-      "english": "Charmander",
-      "japanese": "ヒトカゲ",
-      "chinese": "小火龙"
+    {
+      "name": "charmander",
+      "url": "https://pokeapi.co/api/v2/pokemon/4/"
     },
-    "type": [
-      "Fire"
-    ],
-    "base": {
-      "HP": 39,
-      "Attack": 52,
-      "Defense": 43,
-      "Sp. Attack": 60,
-      "Sp. Defense": 50,
-      "Speed": 65
-    }
-  },
-  {
-    "id": 5,
-    "name": {
-      "english": "Charmeleon",
-      "japanese": "リザード",
-      "chinese": "火恐龙"
+    {
+      "name": "charmeleon",
+      "url": "https://pokeapi.co/api/v2/pokemon/5/"
     },
-    "type": [
-      "Fire"
-    ],
-    "base": {
-      "HP": 58,
-      "Attack": 64,
-      "Defense": 58,
-      "Sp. Attack": 80,
-      "Sp. Defense": 65,
-      "Speed": 80
-    }
-  },
-  {
-    "id": 6,
-    "name": {
-      "english": "Charizard",
-      "japanese": "リザードン",
-      "chinese": "喷火龙"
+    {
+      "name": "charizard",
+      "url": "https://pokeapi.co/api/v2/pokemon/6/"
     },
-    "type": [
-      "Fire",
-      "Flying"
-    ],
-    "base": {
-      "HP": 78,
-      "Attack": 84,
-      "Defense": 78,
-      "Sp. Attack": 109,
-      "Sp. Defense": 85,
-      "Speed": 100
-    }
-  },
-  {
-    "id": 7,
-    "name": {
-      "english": "Squirtle",
-      "japanese": "ゼニガメ",
-      "chinese": "杰尼龟"
+    {
+      "name": "squirtle",
+      "url": "https://pokeapi.co/api/v2/pokemon/7/"
     },
-    "type": [
-      "Water"
-    ],
-    "base": {
-      "HP": 44,
-      "Attack": 48,
-      "Defense": 65,
-      "Sp. Attack": 50,
-      "Sp. Defense": 64,
-      "Speed": 43
-    }
-  },
-  {
-    "id": 8,
-    "name": {
-      "english": "Wartortle",
-      "japanese": "カメール",
-      "chinese": "卡咪龟"
+    {
+      "name": "wartortle",
+      "url": "https://pokeapi.co/api/v2/pokemon/8/"
     },
-    "type": [
-      "Water"
-    ],
-    "base": {
-      "HP": 59,
-      "Attack": 63,
-      "Defense": 80,
-      "Sp. Attack": 65,
-      "Sp. Defense": 80,
-      "Speed": 58
-    }
-  },
-  {
-    "id": 9,
-    "name": {
-      "english": "Blastoise",
-      "japanese": "カメックス",
-      "chinese": "水箭龟"
+    {
+      "name": "blastoise",
+      "url": "https://pokeapi.co/api/v2/pokemon/9/"
     },
-    "type": [
-      "Water"
-    ],
-    "base": {
-      "HP": 79,
-      "Attack": 83,
-      "Defense": 100,
-      "Sp. Attack": 85,
-      "Sp. Defense": 105,
-      "Speed": 78
-    }
-  },
-  {
-    "id": 10,
-    "name": {
-      "english": "Caterpie",
-      "japanese": "キャタピー",
-      "chinese": "绿毛虫"
+    {
+      "name": "caterpie",
+      "url": "https://pokeapi.co/api/v2/pokemon/10/"
     },
-    "type": [
-      "Bug"
-    ],
-    "base": {
-      "HP": 45,
-      "Attack": 30,
-      "Defense": 35,
-      "Sp. Attack": 20,
-      "Sp. Defense": 20,
-      "Speed": 45
-    }
-  },
-  {
-    "id": 11,
-    "name": {
-      "english": "Metapod",
-      "japanese": "トランセル",
-      "chinese": "铁甲蛹"
+    {
+      "name": "metapod",
+      "url": "https://pokeapi.co/api/v2/pokemon/11/"
     },
-    "type": [
-      "Bug"
-    ],
-    "base": {
-      "HP": 50,
-      "Attack": 20,
-      "Defense": 55,
-      "Sp. Attack": 25,
-      "Sp. Defense": 25,
-      "Speed": 30
-    }
-  },
-  {
-    "id": 12,
-    "name": {
-      "english": "Butterfree",
-      "japanese": "バタフリー",
-      "chinese": "巴大蝶"
+    {
+      "name": "butterfree",
+      "url": "https://pokeapi.co/api/v2/pokemon/12/"
     },
-    "type": [
-      "Bug",
-      "Flying"
-    ],
-    "base": {
-      "HP": 60,
-      "Attack": 45,
-      "Defense": 50,
-      "Sp. Attack": 90,
-      "Sp. Defense": 80,
-      "Speed": 70
-    }
-  },
-  {
-    "id": 13,
-    "name": {
-      "english": "Weedle",
-      "japanese": "ビードル",
-      "chinese": "独角虫"
+    {
+      "name": "weedle",
+      "url": "https://pokeapi.co/api/v2/pokemon/13/"
     },
-    "type": [
-      "Bug",
-      "Poison"
-    ],
-    "base": {
-      "HP": 40,
-      "Attack": 35,
-      "Defense": 30,
-      "Sp. Attack": 20,
-      "Sp. Defense": 20,
-      "Speed": 50
-    }
-  },
-  {
-    "id": 14,
-    "name": {
-      "english": "Kakuna",
-      "japanese": "コクーン",
-      "chinese": "铁壳蛹"
+    {
+      "name": "kakuna",
+      "url": "https://pokeapi.co/api/v2/pokemon/14/"
     },
-    "type": [
-      "Bug",
-      "Poison"
-    ],
-    "base": {
-      "HP": 45,
-      "Attack": 25,
-      "Defense": 50,
-      "Sp. Attack": 25,
-      "Sp. Defense": 25,
-      "Speed": 35
-    }
-  },
-  {
-    "id": 15,
-    "name": {
-      "english": "Beedrill",
-      "japanese": "スピアー",
-      "chinese": "大针蜂"
+    {
+      "name": "beedrill",
+      "url": "https://pokeapi.co/api/v2/pokemon/15/"
     },
-    "type": [
-      "Bug",
-      "Poison"
-    ],
-    "base": {
-      "HP": 65,
-      "Attack": 90,
-      "Defense": 40,
-      "Sp. Attack": 45,
-      "Sp. Defense": 80,
-      "Speed": 75
-    }
-  },
-  {
-    "id": 16,
-    "name": {
-      "english": "Pidgey",
-      "japanese": "ポッポ",
-      "chinese": "波波"
+    {
+      "name": "pidgey",
+      "url": "https://pokeapi.co/api/v2/pokemon/16/"
     },
-    "type": [
-      "Normal",
-      "Flying"
-    ],
-    "base": {
-      "HP": 40,
-      "Attack": 45,
-      "Defense": 40,
-      "Sp. Attack": 35,
-      "Sp. Defense": 35,
-      "Speed": 56
-    }
-  },
-  {
-    "id": 17,
-    "name": {
-      "english": "Pidgeotto",
-      "japanese": "ピジョン",
-      "chinese": "比比鸟"
+    {
+      "name": "pidgeotto",
+      "url": "https://pokeapi.co/api/v2/pokemon/17/"
     },
-    "type": [
-      "Normal",
-      "Flying"
-    ],
-    "base": {
-      "HP": 63,
-      "Attack": 60,
-      "Defense": 55,
-      "Sp. Attack": 50,
-      "Sp. Defense": 50,
-      "Speed": 71
-    }
-  },
-  {
-    "id": 18,
-    "name": {
-      "english": "Pidgeot",
-      "japanese": "ピジョット",
-      "chinese": "大比鸟"
+    {
+      "name": "pidgeot",
+      "url": "https://pokeapi.co/api/v2/pokemon/18/"
     },
-    "type": [
-      "Normal",
-      "Flying"
-    ],
-    "base": {
-      "HP": 83,
-      "Attack": 80,
-      "Defense": 75,
-      "Sp. Attack": 70,
-      "Sp. Defense": 70,
-      "Speed": 101
-    }
-  },
-  {
-    "id": 19,
-    "name": {
-      "english": "Rattata",
-      "japanese": "コラッタ",
-      "chinese": "小拉达"
+    {
+      "name": "rattata",
+      "url": "https://pokeapi.co/api/v2/pokemon/19/"
     },
-    "type": [
-      "Normal"
-    ],
-    "base": {
-      "HP": 30,
-      "Attack": 56,
-      "Defense": 35,
-      "Sp. Attack": 25,
-      "Sp. Defense": 35,
-      "Speed": 72
-    }
-  },
-  {
-    "id": 20,
-    "name": {
-      "english": "Raticate",
-      "japanese": "ラッタ",
-      "chinese": "拉达"
+    {
+      "name": "raticate",
+      "url": "https://pokeapi.co/api/v2/pokemon/20/"
     },
-    "type": [
-      "Normal"
-    ],
-    "base": {
-      "HP": 55,
-      "Attack": 81,
-      "Defense": 60,
-      "Sp. Attack": 50,
-      "Sp. Defense": 70,
-      "Speed": 97
-    }
-  },
-  {
-    "id": 21,
-    "name": {
-      "english": "Spearow",
-      "japanese": "オニスズメ",
-      "chinese": "烈雀"
+    {
+      "name": "spearow",
+      "url": "https://pokeapi.co/api/v2/pokemon/21/"
     },
-    "type": [
-      "Normal",
-      "Flying"
-    ],
-    "base": {
-      "HP": 40,
-      "Attack": 60,
-      "Defense": 30,
-      "Sp. Attack": 31,
-      "Sp. Defense": 31,
-      "Speed": 70
-    }
-  },
-  {
-    "id": 22,
-    "name": {
-      "english": "Fearow",
-      "japanese": "オニドリル",
-      "chinese": "大嘴雀"
+    {
+      "name": "fearow",
+      "url": "https://pokeapi.co/api/v2/pokemon/22/"
     },
-    "type": [
-      "Normal",
-      "Flying"
-    ],
-    "base": {
-      "HP": 65,
-      "Attack": 90,
-      "Defense": 65,
-      "Sp. Attack": 61,
-      "Sp. Defense": 61,
-      "Speed": 100
-    }
-  },
-  {
-    "id": 23,
-    "name": {
-      "english": "Ekans",
-      "japanese": "アーボ",
-      "chinese": "阿柏蛇"
+    {
+      "name": "ekans",
+      "url": "https://pokeapi.co/api/v2/pokemon/23/"
     },
-    "type": [
-      "Poison"
-    ],
-    "base": {
-      "HP": 35,
-      "Attack": 60,
-      "Defense": 44,
-      "Sp. Attack": 40,
-      "Sp. Defense": 54,
-      "Speed": 55
-    }
-  },
-  {
-    "id": 24,
-    "name": {
-      "english": "Arbok",
-      "japanese": "アーボック",
-      "chinese": "阿柏怪"
+    {
+      "name": "arbok",
+      "url": "https://pokeapi.co/api/v2/pokemon/24/"
     },
-    "type": [
-      "Poison"
-    ],
-    "base": {
-      "HP": 60,
-      "Attack": 95,
-      "Defense": 69,
-      "Sp. Attack": 65,
-      "Sp. Defense": 79,
-      "Speed": 80
+    {
+      "name": "pikachu",
+      "url": "https://pokeapi.co/api/v2/pokemon/25/"
     }
-  },
-  {
-    "id": 25,
-    "name": {
-      "english": "Pikachu",
-      "japanese": "ピカチュウ",
-      "chinese": "皮卡丘"
-    },
-    "type": [
-      "Electric"
-    ],
-    "base": {
-      "HP": 35,
-      "Attack": 55,
-      "Defense": 40,
-      "Sp. Attack": 50,
-      "Sp. Defense": 50,
-      "Speed": 90
-    }
-  },
-  {
-  }]
+  ]
