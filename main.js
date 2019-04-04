1022ca3943d97ed8a467fde2f1f1d133ac247b80
camcatwriter <theothertotalbookworm@gmail.com>
2019-04-04 16:45:11 -0600

    sprites come through

--- main.js
+++ main.js
@@ -20,8 +20,11 @@ const mainContainer = document.querySelector('.container')
 function createPokeCard(pokedata) {
   let card = document.createElement('div')
   let name = document.createElement('p')
+  let image = document.createElement('img')
 
   name.textContent = pokedata.name
+  image.src = pokedata.sprites.front_default
   card.appendChild(name)
+  card.appendChild(image)
   mainContainer.appendChild(card)
 }
