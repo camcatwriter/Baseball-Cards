65192bf1f2057a2ebcba107817a11f71f4269c0a
camcatwriter <theothertotalbookworm@gmail.com>
2019-04-04 17:53:34 -0600

    fixed color of navbar and hover

--- main.js
+++ main.js
@@ -6,18 +6,19 @@ pokemon.forEach((singleMon) => {
     return response.json();
   })
   .then(function(myJson) {
-    console.log(myJson) ;
+    // console.log(myJson) ;
     createPokeCard(myJson)
   })
 })
 
 
 
-console.log(pokemon)
+// console.log(pokemon)
 
 const mainContainer = document.querySelector('.container')
 
 function createPokeCard(pokeData) {
+  console.log (pokeData.id)
   let card = document.createElement('div')
   let figure = document.createElement('figure')
   let name = document.createElement('figcaption')
@@ -25,7 +26,7 @@ function createPokeCard(pokeData) {
 
   let upperName = pokeData.name.charAt(0).toUpperCase() + pokeData.name.slice(1)
   name.textContent = upperName
-  image.src = pokeData.sprites.front_default
+  image.src = `../images/${pokeData.id}${upperName}.png`
   figure.appendChild(image)
   figure.appendChild(name)
   card.appendChild(figure)
