bd0fb446acb0d1e69c0b235dcae62008bb7c528a
camcatwriter <theothertotalbookworm@gmail.com>
2019-04-04 16:40:17 -0600

    pokemon forEach loop funcitoning

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
 import { pokemon } from './pokemon.js'
 
-fetch('https://pokeapi.co/api/v2/pokemon/1')
+pokemon.forEach((singleMon) => {
+  fetch(singleMon.url)
   .then(function(response) {
     return response.json();
   })
   .then(function(myJson) {
     console.log(myJson) ;
+    createPokeCard(myJson)
   })
+})
+
+
 
 console.log(pokemon)
 
+const mainContainer = document.querySelector('.container')
+
 function createPokeCard(pokedata) {
-  
+  let card = document.createElement('div')
+  let name = document.createElement('p')
+
+  name.textContent = pokedata.name
+  card.appendChild(name)
+  mainContainer.appendChild(card)
 }
