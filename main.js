8a704f0f9bcec8418a3bfd1cd5f46d38549cc983
camcatwriter <theothertotalbookworm@gmail.com>
2019-04-06 22:41:37 -0600

    added code from working file

--- main.js
+++ main.js
@@ -2,32 +2,71 @@ import { pokemon } from './pokemon.js'
 
 const mainContainer = document.querySelector('.container')
 
-function createPokeCard(pokeData) {
-  console.log (pokeData.id)
-  let card = document.createElement('div')
-  let figure = document.createElement('figure')
-  let name = document.createElement('figcaption')
-  let image = document.createElement('img')
-
-  let upperName = pokeData.name.charAt(0).toUpperCase() + pokeData.name.slice(1)
-  name.textContent = upperName
-  if(pokeData.id !==0) {
-    image.src = '../images/${pokeData.id}${upperName}.png'
-  } else {
-    image.src = '../images/pokeball.png'
-  }
- 
-  figure.appendChild(image)
-  figure.appendChild(name)
-  mainContainer.appendChild(card)
-}
-
 function cardFront(pokeData) {
   let cardFront = document.createElement('div')
   cardFront.className = 'card_face'
   let figure = document.createElement('figure')
   let name = document.createElement('figcaption')
   let image = document.createElement('img') 
+  
+  name.textContent = upperName
+  if(pokeData.id !==0) {
+    image.src = '../images/${pokeData.id}${upperName}.png'
+  } else {
+    image.src = '../images/pokeball.png'
+  }
+
+  figure.appendChild(image)
+  figure.appendChild(name)
+  cardFront.appendChild(figure)
+  return cardFront
+}
+
+function cardBackInfo(pokeData) {
+  let infoDiv = document.createElement('div')
+  infoDiv.className = 'infoDiv'
+  let moveOne = document.createElement('p')
+  let moveTwo = document.createElement('p')
+  let moveThree = document.createElement('p')
+  let moveFour = document.createElement('p')
+  moveOne.textContent = pokeData.moves[0].move.name
+  moveTwo.textContent = pokeData.moves[1].move.name
+  moveThree.textContent = pokeData.moves[2].move.name
+  moveFour.textContent = pokeData.moves[3].move.name
+  infoDiv.appendChild(moveOne)
+  infoDiv.appendChild(moveTwo)
+  infoDiv.appendChild(moveThree)
+  infoDiv.appendChild(moveFour)
+  return infoDiv
+}
+
+function cardBack(pokeData) {
+  let cardBack = document.createElement('div')
+  let backImage = document.createElement('img')
+  backImage.className = 'backImage'
+  backImage.src = `../images/pokecardBack.png`
+  cardBack.className = 'card_face card_face--back'
+  cardBack.appendChild(backImage)
+  cardBack.appendChild(cardBackInfo(pokeData))
+  return cardBack
+}
+
+function createPokeCard(pokeData) {
+  console.log (pokeData.id)
+  let scene = document.createElement('div')
+  scene.className = 'scene'
+  let card = document.createElement('div')
+  card.className = 'card'
+
+  card.appendChild(cardFront(pokeData))
+  card.appendChild(cardBack(pokeData))
+  
+  card.addEventListener( 'click', function() {
+    card.classList.toggle('is-flipped');
+  })
+
+  scene.appendChild(card)
+  mainContainer.appendChild(scene)
 }
 
 pokemon.forEach((singleMon) => {
@@ -37,7 +76,7 @@ pokemon.forEach((singleMon) => {
   })
   .then(function(myJson) {
     // console.log(myJson) ;
-    createPokeCard((myJson))
+    createPokeCard(matchIdToImage(myJson))
   })
 })
 
@@ -56,6 +95,16 @@ function matchIdToImage(aPokemon) {
   }
   aPokemon.name = aPokemon.name.charAt(0).toUpperCase() + aPokemon.name.slice(1)
   return aPokemon
+}
+
+function fetchExistingPokemon(id) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function(retrievedPokemon) {
+    createPokeCard(matchIdToImage(retrievedPokemon))
+  })
 }
 
 class Pokemon {
